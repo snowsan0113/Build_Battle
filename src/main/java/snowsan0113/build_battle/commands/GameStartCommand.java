47e280e637e2ac455a400440faf35ccaf5c5cfe9
snowsan0113 <egawayuuki0113@gmail.com>
2025-06-24 22:29:39 +0900

    【追加】ゲームとして開始できるように追加

--- src/main/java/snowsan0113/build_battle/commands/GameStartCommand.java
+++ src/main/java/snowsan0113/build_battle/commands/GameStartCommand.java
@@ -4,13 +4,21 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import snowsan0113.build_battle.BuildBattle;
+import snowsan0113.build_battle.manager.GameManager;
+
+import java.io.IOException;
 
 public class GameStartCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender send, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
         if (cmd.getName().equalsIgnoreCase("buildbattle_start")) {
-
+            try {
+                GameManager.getInstance().startGame();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         return false;
     }
--- src/main/java/snowsan0113/build_battle/manager/GameManager.java
+++ src/main/java/snowsan0113/build_battle/manager/GameManager.java
@@ -1,13 +1,19 @@
 package snowsan0113.build_battle.manager;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import snowsan0113.build_battle.BuildBattle;
 import snowsan0113.build_battle.util.ChatUtil;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
 
 public class GameManager {
 
@@ -17,17 +23,31 @@ public class GameManager {
     //ゲーム
     private BukkitTask task; //ゲームタスク
     private GameStatus status; //ゲームの状態
+    private int MAX_BUILD_TIME;
     private int build_time; //建築する残り時間
     private int count_time; //カウント時間
 
+    //ボスバー
+    private double MAX_BOSSBAR;
+    private BossBar bossBar;
+
+    //建築物
+    private BuildManager.Build now_build; //現在の建築物
+    private final List<BuildManager.Build> build_list; //残りの建築物
+
     //建築プレイヤー
     private OfflinePlayer build_player; //建築しているプレイヤー
-    private Set<OfflinePlayer> build_player_list; //建築予定のプレイヤー
+    private List<OfflinePlayer> build_player_list; //建築予定のプレイヤー
 
-    private GameManager() {
+    private GameManager() throws IOException {
+        this.count_time = 10;
+        this.MAX_BUILD_TIME = 60*5;
         this.build_time = 60*5;
-        this.build_player_list = new HashSet<>();
+        this.build_player_list = new ArrayList<>();
+        this.build_list = new ArrayList<>(BuildManager.getInstance().getBuildList());
         this.status = GameStatus.WAIITNG;
+        this.bossBar = Bukkit.createBossBar("残り時間", BarColor.BLUE, BarStyle.SEGMENTED_6);
+        this.MAX_BOSSBAR = bossBar.getProgress();
     }
 
     public int startGame() {
@@ -36,8 +56,10 @@ public class GameManager {
                 @Override
                 public void run() {
                     if (status == GameStatus.WAIITNG || status == GameStatus.CONNTING) {
-                        if (count_time <= 0) {
+                        if (count_time == 0) {
                             ChatUtil.sendGlobalMessage("ゲーム開始!");
+                            build_player_list.addAll(Bukkit.getOnlinePlayers());
+                            nextGame();
                             status = GameStatus.RUNNING;
                         }
                         else {
@@ -48,6 +70,32 @@ public class GameManager {
                     }
                     else if (status == GameStatus.RUNNING) {
 
+                        for (Player online : Bukkit.getOnlinePlayers()) {
+                            if (!bossBar.getPlayers().contains(online)) {
+                                bossBar.addPlayer(online);
+                            }
+                        }
+                        bossBar.setProgress(MAX_BOSSBAR * ((double) build_time / MAX_BUILD_TIME));
+                        bossBar.setTitle("残り時間：" + build_time);
+
+                        if (build_time == 300) {
+                            ChatUtil.sendGlobalMessage("=== ヒント!! ===" + "\n" +
+                                    "難しさは" + ChatColor.YELLOW + now_build.difficulty());
+                        }
+                        if (build_time == 200) {
+                            ChatUtil.sendGlobalMessage("=== ヒント!! ===" + "\n" +
+                                    "ジャンルは" + ChatColor.YELLOW + now_build.genre());
+                        }
+                        if (build_time == 100) {
+                            ChatUtil.sendGlobalMessage("=== ヒント!! ===" + "\n" +
+                                    "ヒントは" + ChatColor.YELLOW + now_build.hint());
+                        }
+
+                        if (build_time == 0) {
+                            nextGame();
+                        }
+
+                        build_time--;
                     }
                 }
             }.runTaskTimer(BuildBattle.getPlugin(BuildBattle.class), 0L, 20L);
@@ -63,6 +111,24 @@ public class GameManager {
 
     }
 
+    public void nextGame() {
+        //建築する人を選ぶ
+        Collections.shuffle(build_player_list);
+        build_player = build_player_list.get(0);
+        build_player_list.remove(build_player);
+
+        //建築物を選ぶ
+        Collections.shuffle(build_list);
+        now_build = build_list.get(0);
+        build_list.remove(now_build);
+
+        ChatUtil.sendMessage(build_player.getPlayer(), "==============" + "\n" +
+                "あなたが建築する人に選ばれました。 次のものを建築してください。\n" +
+                "・" + now_build.name() + "\n" +
+                "==============");
+
+    }
+
     public int getTime() {
         return build_time;
     }
@@ -71,11 +137,15 @@ public class GameManager {
         this.build_time = build_time;
     }
 
-    public static GameManager getInstance() {
+    public static GameManager getInstance() throws IOException {
         if (instance == null) {
             instance = new GameManager();
         }
         return instance;
+    }
+
+    public List<BuildManager.Build> getBuild_list() {
+        return build_list;
     }
 
     public enum GameStatus {
